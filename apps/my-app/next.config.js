fe9770970c51314f61de42f5e2b823850e041d69
Lido.lee <lido.lee@psp-power.com.tw>
2023-09-25 23:52:26 +0800

    integrating redux-toolkit

--- apps/my-app/next.config.js
+++ apps/my-app/next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  output: "standalone",
+
+  experimental: {
+    transpilePackages: ["ui", "redux-utils"],
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
+};
